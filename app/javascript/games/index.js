a18186b69fbf5d26e06e375b19ab685132949760
fujikan <69848595+fujikan@users.noreply.github.com>
2020-10-14 18:58:00 +0900

    ログイン画面、新規登録画面の実装

---
+++ app/javascript/games/index.js
@@ -0,0 +1,11 @@
+// window.addEventListener('load', function(){
+//   const pullDownButton = document.getElementById("div")
+
+//   pullDownButton.addEventListener('mouseover', function(){
+//     this.setAttribute('style', "color: blue")
+//   })
+
+//   pullDownButton.addEventListener('mouseout', function(){
+//     this.setAttribute('style', "color: red")
+//   })  
+// })
