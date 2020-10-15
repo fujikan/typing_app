e31a01712a91abbb686ca3ece6c4bc47fa47d78a
fujikan <69848595+fujikan@users.noreply.github.com>
2020-10-15 12:37:46 +0900

    ビューの編集

--- app/javascript/games/index.js
+++ app/javascript/games/index.js
@@ -1,11 +1,11 @@
-// window.addEventListener('load', function(){
-//   const pullDownButton = document.getElementById("div")
+//  window.addEventListener('load', function(){
+//    const pullDownButton = document.getElementById("user")
 
-//   pullDownButton.addEventListener('mouseover', function(){
-//     this.setAttribute('style', "color: blue")
-//   })
+//    pullDownButton.addEventListener('mouseover', function(){
+//      this.setAttribute('style', "backgrouund-color: gainsboro")
+//    })
 
-//   pullDownButton.addEventListener('mouseout', function(){
-//     this.setAttribute('style', "color: red")
-//   })  
-// })
+//    pullDownButton.addEventListener('mouseout', function(){
+//      this.setAttribute('style', "color: white")
+//    })  
+//  })
